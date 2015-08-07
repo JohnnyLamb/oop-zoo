6633c404be366cd937cb801e75baa78b1b3f76e7
johnny lamb <agnusjack@gmail.com>
2015-08-06 20:52:03 -0600

    finished zoo methods

--- src/zoo.js
+++ src/zoo.js
@@ -17,6 +17,29 @@ Zoo.prototype.open = function(){
   this.status = open;
 };
 
+Zoo.prototype.closed = function(){
+  this.status = closed;
+};
 
+Zoo.prototype.isOpen = function(){
+  if(this.status === open){
+      return "Open!";
+  }
+};
+
+Zoo.prototype.addAnimal = function(Animal){
+ if(this.status === open){
+  this.animals.push(new Animal());
+ }
+};
+
+Zoo.prototype.removeAnimal = function(animal){
+  if(this.status === open){
+      this.animals.indexOf(animal);
+      for(i = 0; i < this.animal.length; i++){
+        this.animal.splice(animal,1);
+      }
+  }
+};
 
 module.exports = Zoo;
