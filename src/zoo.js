b2e8685dc2a43a3e1de174b66c56044b5ae1d3ad
johnny lamb <agnusjack@gmail.com>
2015-08-06 20:39:51 -0600

    more props and methods

--- src/zoo.js
+++ src/zoo.js
@@ -5,6 +5,18 @@ function Zoo(name, location){
   this.location = location;
   this.status = closed;
   this.animals = [];
+
 }
+
+
+Zoo.prototype.changeLocation = function(location){
+  this.location = location;
+};
+
+Zoo.prototype.open = function(){
+  this.status = open;
+};
+
+
 
 module.exports = Zoo;
